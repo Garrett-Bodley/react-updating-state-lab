2e0325ffff20fdc4bab67bb70cff50d029bacfe9
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-04 17:53:49 -0500

    commit tree cdae37cc982ab33be633c0d9c8d06fe5915829ea

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,21 @@
 // Code DigitalClicker Component Here
+import React, {Component} from 'react'
+
+export default class DigitalClicker extends Component {
+  constructor(){
+    super();
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState(prevState => {
+      return {timesClicked: prevState.timesClicked + 1}
+    })
+  }
+
+  render(){
+    return(<button onClick={this.incrementCounter}>{this.state.timesClicked}</button>)
+  }
+}
